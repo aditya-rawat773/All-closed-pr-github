c9c3d316de0a51aeda7c49a4dc4c71fe7ba78c1e
Aditya Kumar Rawat <aditya.rawat@glance.com>
2022-07-24 17:11:35 +0530

    date format and ui fix

--- app/src/main/java/com/example/allclosedprgithub/util/Helper.kt
+++ app/src/main/java/com/example/allclosedprgithub/util/Helper.kt
@@ -1,5 +1,17 @@
 package com.example.allclosedprgithub.util
 
-class Helper {
+import java.text.SimpleDateFormat
+import java.util.*
+
+object Helper {
+
+     fun parseDateTime(date:String):String{
+
+        val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault()) // the initial pattern
+        val formatter = SimpleDateFormat("dd MMMM yyyy, hh:mm a", Locale.getDefault()) // the desired output pattern
+
+        val parse = parser.parse(date)
+        return  formatter.format(parse!!)
+    }
 
 }
